f24a36287b315139fe48c74c13442aae34abf23f
binggozju <zhanyb.cs2012@gmail.com>
2015-12-26 14:21:20 +0800

    clear some unused files

--- Makefile
+++ Makefile
@@ -1,7 +1,31 @@
-all: webhttpd
+#all: webhttpd
+#webhttpd: webhttpd.cpp options_parser.cpp config.cpp utils.cpp logging.cpp
+#	g++ -O0 -g -Wall webhttpd.cpp options_parser.cpp config.cpp utils.cpp logging.cpp  -ljson -llog4cplus -lpthread -o webhttpd
+#clean:
+#	rm webhttpd
 
-webhttpd: webhttpd.cpp options_parser.cpp config.cpp utils.cpp logging.cpp
-	g++ -O0 -g -Wall webhttpd.cpp options_parser.cpp config.cpp utils.cpp logging.cpp  -ljson -llog4cplus -lpthread -o webhttpd
+TARGET = webhttpd
+
+INCLUDES = -I/usr/local/include
+LIBS = -L/usr/local/lib
+
+CXX = g++
+CFLAGS = -g -Wall -O0 -c $(INCLUDES) 
+LDFLAGS = -ljson -llog4cplus -lpthread -levent $(LIBS)
+
+SRCDIR = .
+SRCS = $(wildcard $(SRCDIR)/*.cpp)
+OBJS = $(addsuffix .o, $(basename $(SRCS)))
+HEADS = $(addsuffix .h, $(basename $(SRCS)))
+
+
+all: $(TARGET)
+
+$(TARGET): $(OBJS)
+	$(CXX) $(LDFLAGS) $^ -o $@
+
+%.o: %.cpp
+	$(CXX) $(CFLAGS) $< -o $@
 
 clean:
-	rm webhttpd
+	rm -rf *.o $(TARGET)
--- webhttpd.cpp
+++ webhttpd.cpp
@@ -1,16 +1,37 @@
+#include <sys/socket.h>
+#include <unistd.h>
+
 #include <iostream>
 #include <string>
+#include <vector>
+
+#include <event2/event.h>
 
 #include "options_parser.h"
 #include "config.h"
 #include "logging.h"
+
+namespace webhttpd {
+	/* 全局变量 */
+	OptionsParser* opt_parser;
+	// TODO:ConfigParser* conf_parser;
+	// TODO:日志记录器
+
+	struct event_base *base;
+
+	std::vector<int> child_fds; // 存储master进程用来与各worker进程通信的unix域套接字
+	int child_index; // master进程将向child_fds[child_index]写入新的连接套接字，传递给对应的worker子进程 
+	
+	int parent_fd; // 在worker子进程中存储的是用来与master进程通信的unix域套接字，在master进程中为-1
+}
+
 using namespace webhttpd;
 
 int main(int argc, char** argv) {
 	std::string conf_file_name = "conf/conf.json"; // 默认的配置文件
 	
 	/* 处理命令行选项 */
-	OptionsParser* opt_parser = OptionsParser::GetInstance();
+	opt_parser = OptionsParser::GetInstance();
 	opt_parser->Parse(argc, argv);
 
 	if("" != opt_parser->GetOptionVal('h')) {
@@ -37,12 +58,28 @@ int main(int argc, char** argv) {
 	logger_master.Debug("This is a debug message");
 	logger_master.Info("This is a info message");
 	logger_master.Warn("This is a warn message");
-	logger_master.Error("This is a error message");
+	//logger_master.Error("This is a error message");
 
+
+	// TODO: 信号处理
 	// TODO: 创建多进程，基于libevent来处理http请求
+	/*
+	int worker_num = conf_parser.GetIntItem("common| worker_processes");
+	logger_master.Info("webhttp will start xxx worker process"); // TODO: 修改接口
+
+	for(int i = 0; i < worker_num; ++i) {
+		int sockfd[2];
+		if(0 != socketpair(AF_LOCAL, SOCK_STREAM, 0, sockfd)) {
+			logger_master.Error(""); // TODO
+			return -1;
+		}
+		pid_t pid = fork();
+		// TODO
 
 
 
+	}
+	*/
 
 	delete opt_parser;
 
