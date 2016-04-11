b641aeef46f475e5f9d0533759105473c04c9616
binggozju <zhanyb.cs2012@gmail.com>
2016-04-11 13:50:20 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # webhttpd
 
 TODO:
-- realize a easy http server based on libevent;
 - fix the hyperlink problem in the web page;
 - realise a master process and multiple worker process mode;
 - add a task pool for every worker process;
--- webhttpd.cpp
+++ webhttpd.cpp
@@ -2,7 +2,6 @@
  * a easy http server based on libevent.
  * 
  * author:	binggozju
- * date:	2015-12-29
  */
 
 #include "webhttpd.h"
@@ -14,52 +13,48 @@
 struct event_base *base;
 struct evhttp *http;
 struct evhttp_bound_socket *handle;
-//string uri_root[512]; 
+//string uri_root[512];
 
-vector<int> child_fds; // 存储master进程用来与各worker进程通信的unix域套接字
-int child_index; // master进程将向child_fds[child_index]写入新的连接套接字，传递给对应的worker子进程 
-int parent_fd; // 在worker子进程中存储的是用来与master进程通信的unix域套接字，在master进程中为-1
+// 存储master进程用来与各worker进程通信的unix域套接字
+vector<int> child_fds;
+// master进程将向child_fds[child_index]写入新的连接套接字，传递给对应的worker子进程 
+int child_index;
+// 在worker子进程中存储的是用来与master进程通信的unix域套接字，在master进程中为-1
+int parent_fd;
 
 OptionsParser* opt_parser;
 ConfigParser conf_parser;
 Logging logger;
 
 char uri_root[512];
-
-const struct table_entry {
-	const char *extension;
-	const char *content_type;
-} content_type_table[] = {
-	{ "txt", "text/plain" },
-	{ "c", "text/plain" },
-	{ "h", "text/plain" },
-	{ "html", "text/html" },
-	{ "htm", "text/htm" },
-	{ "css", "text/css" },
-	{ "gif", "image/gif" },
-	{ "jpg", "image/jpeg" },
-	{ "jpeg", "image/jpeg" },
-	{ "png", "image/png" },
-	{ "pdf", "application/pdf" },
-	{ "ps", "application/postsript" },
-	{ NULL, NULL },
+content_type_t content_type_table[] = {
+	{"txt",		"text/plain"},
+	{"html",	"text/plain"},
+	{"htm",		"text/plain"},
+	{"gif",		"image/gif"},
+	{"jpg",		"image/jpeg"},
+	{"jpeg",	"image/jpeg"},
+	{"png",		"image/png"},
+	{"pdf",		"application/pdf"},
+	{"",		""},
 };
 
 /* Try to guess a good content-type for 'path' */
-const char* guess_content_type(const char *path)
+string guess_content_type(char *path)
 {
-	const char *last_period, *extension;
-	const struct table_entry *ent;
+	char *last_period, *extension;
+	struct content_type_t *ent;
 	last_period = strrchr(path, '.');
+
 	if (!last_period || strchr(last_period, '/'))
-		goto not_found; /* no exension */
+		return "application/misc";
+	
 	extension = last_period + 1;
-	for (ent = &content_type_table[0]; ent->extension; ++ent) {
-		if (!evutil_ascii_strcasecmp(ent->extension, extension))
-			return ent->content_type;
+	for(ent = &content_type_table[0]; ent->extension != ""; ++ent) {
+		if (!evutil_ascii_strcasecmp(ent->extension.c_str(), extension))
+			return ent->type;
 	}
 
-not_found:
 	return "application/misc";
 }
 
@@ -206,7 +201,7 @@ void send_document_cb(struct evhttp_request *req, void *arg)
 	} else {	// 如果用户请求的是一个文件
 		/* Otherwise it's a file; add it to the buffer to get
 		 * sent via sendfile */
-		const char *type = guess_content_type(decoded_path);
+		const char *type = guess_content_type(decoded_path).c_str();
 		if ((fd = open(whole_path, O_RDONLY)) < 0) {
 			perror("open");
 			goto err;
